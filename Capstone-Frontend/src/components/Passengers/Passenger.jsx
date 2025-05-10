1958542a1e20eba0db07202dc07580c1b14bc2d6
Manogari Kumaraguru <manogarikumaraguru@gmail.com>
2025-05-09 23:03:11 -0400

    addpasseger

--- Capstone-Frontend/src/components/Passengers/Passenger.jsx
+++ Capstone-Frontend/src/components/Passengers/Passenger.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { createPassenger } from '../../services/passengers-api-jsx';
 export default function Passenger()
 {
     const AddPassenger = (e)=>
     {
-        
+        e.preventDefault();
+        const passenger = {
+            firstName: e.target.name.value, 
+            color: e.target.color.value,
+            readyToEat: e.target.readyToEat.checked }
+            createPassenger(passenger).then(()=>{
+              nav('/')
+            })
     }
+    
     const nav = useNavigate();
     return (
         <>
         <div>Add a passenger</div>
         <form onSubmit={AddPassenger}>
-          Name: <input type='text' name='name'></input><br/>
-          Color: <input type='text' name='color'></input><br/>
-          Ready to Eat: <input type='checkbox' name='readyToEat'></input><br/>
+          FirstName: <input type='text' name='name'></input><br/>
+          LastName: <input type='text' name='color'></input><br/>
+          Email: <input type='checkbox' name='readyToEat'></input><br/>
+          Age : <input type='number' name='age'></input><br/>
           <input type='submit'></input>
         </form>
         </>
